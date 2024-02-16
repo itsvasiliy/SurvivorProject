983c2a66a6495138bdd56cf6f4e7b669e6eb27ce
Yuriy Balint <yurabalint0103@gmail.com>
2024-02-16 15:45:44 +0200

    Added PlayerState injection

---
+++ Assets/Scripts/GameCore/Player/Player States/IPlayerStateController.cs
@@ -0,0 +1,13 @@
+public interface IPlayerStateController
+{
+    public void SetState(PlayerStates state);
+    public PlayerStates GetState();
+}
+
+
+public enum PlayerStates
+{
+    Idle,
+    Running,
+    Mining,
+}
