f38dd582a4536aed7f128efab5040602338e8777
Yuriy Balint <yurabalint0103@gmail.com>
2024-02-17 22:19:27 +0200

    Collectible items rescripting

--- Assets/Scripts/GameCore/Resources/CollectibleIItems/CollectItemRising.cs
+++ Assets/Scripts/GameCore/Resources/CollectibleIItems/CollectItemRising.cs
@@ -6,18 +6,12 @@ public class CollectItemRising : MonoBehaviour
     [SerializeField] float riseSpeed = 1f;
     [SerializeField] float riseHeight = 1f;
     [SerializeField] float lifeTime = 2f;
-    [SerializeField] int amount;
     [SerializeField] TextMeshPro textAmount;
 
+    private int amount;
 
-    private void Start()
-    {
-        textAmount.text = $"+{amount}";
-        Destroy(gameObject, lifeTime);
-    }
 
-    void Update()
-    {
-        transform.Translate(Vector3.up * riseSpeed * Time.deltaTime);
-    }
+    private void Start() => Destroy(gameObject, lifeTime);
+    public void SetAmount(int amount) => textAmount.text = $"+{amount}";
+    void Update() => transform.Translate(Vector3.up * riseSpeed * Time.deltaTime);
 }
--- Assets/Scripts/GameCore/Resources/CollectibleIItems/CollectibleItem.cs
+++ Assets/Scripts/GameCore/Resources/CollectibleIItems/CollectibleItem.cs
@@ -1,5 +1,6 @@
 using Assets.Scripts.GameCore.Interfaces;
 using Assets.Scripts.GameCore.Resources;
+using TMPro;
 using UnityEngine;
 using Zenject;
 
@@ -8,17 +9,58 @@ public class CollectibleItem : MonoBehaviour
     [SerializeField] private ResourceTypes type;
     [SerializeField] GameObject collectionDisplayPrefab;
     [SerializeField] int amount;
+    [SerializeField] Color gizmoColor = Color.red;
+    [Range(5, 20)] public float detectionRange = 20;
 
-   [Inject] readonly IResourceController resourceController;
+
+    [Inject] readonly IResourceController resourceController;
+
+    Vector3 castShape = new Vector3(0.5f, 0.1f, 0.5f);
+    private RaycastHit[] hits;
+    private float moveSpeed = 3f;
+
+
+    private void Update()
+    {
+
+        hits = Physics.BoxCastAll(transform.position, castShape * detectionRange / 2, transform.forward, transform.rotation, detectionRange);
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.TryGetComponent<PlayerController>(out PlayerController player))
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+
 
     public void OnTriggerEnter(Collider collision)
     {
         if (collision.tag == "Player")
-        {
-            Instantiate(collectionDisplayPrefab, transform.position, transform.rotation);
-            resourceController.AddResource(type, amount);
-            Destroy(gameObject);
-        }
+            CollectItem();
+    }
+
+    public void CollectItem()
+    {
+        var collectItemRising = Instantiate(collectionDisplayPrefab, transform.position, 
+            transform.rotation).GetComponent<CollectItemRising>();
+        collectItemRising.SetAmount(amount);
+
+        resourceController.AddResource(type, amount);
+        Destroy(gameObject);
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = gizmoColor;
+        var boxsize = castShape.x * detectionRange;
+        Gizmos.DrawCube(transform.position, new Vector3(boxsize, 0.05f, boxsize));
     }
 
 }
