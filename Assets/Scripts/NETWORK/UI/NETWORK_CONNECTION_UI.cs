b11486397092f5b83c83bdf32661db3df36da86d
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-02-25 15:42:50 -0800

    Network connection menu set up

--- Assets/Scripts/NETWORK/UI/NETWORK_CONNECTION_UI.cs
+++ Assets/Scripts/NETWORK/UI/NETWORK_CONNECTION_UI.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 
 public class NETWORK_CONNECTION_UI : MonoBehaviour
 {
+    [SerializeField] private GameObject accessNetworkElement;
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        accessNetworkElement.SetActive(false);
     }
 
     public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        accessNetworkElement.SetActive(false);
     }
 }
