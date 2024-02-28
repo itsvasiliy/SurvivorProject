e68e8aba85864dcb8e404a9b624e2bbb57ab9274
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-02-27 19:22:29 -0800

    Fixed sync issues

---
+++ Assets/Scripts/NETWORK/PlayerActivities/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] private FloatingJoystick joystick;
+
+    [SerializeField] private Transform playerTransform;
+
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] Animator animator;
+
+    [SerializeField] float speed;
+
+    private void Start()
+    {
+        if (IsOwner == false)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity = new Vector3(joystick.Horizontal * speed, _rigidbody.angularVelocity.y * speed, joystick.Vertical * speed);
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+        {
+            playerTransform.rotation = Quaternion.LookRotation(_rigidbody.linearVelocity);
+            animator.SetBool("IsRunning", true);
+
+            //   playerStateController.SetState(PlayerStates.Running);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", false);
+
+            //  playerStateController.SetState(PlayerStates.Idle); //wrong behavior
+        }
+    }
+}
