37b88c497ac72cc1e5932ed3a1d70450056383b3
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-03-05 19:00:35 -0800

    Fixed player target detection system

--- Assets/Scripts/GameCore/Player/PlayerActivities/OLD_ONE/PlayerShooting.cs
+++ Assets/Scripts/GameCore/Player/PlayerActivities/OLD_ONE/PlayerShooting.cs
@@ -1,86 +1,172 @@
 using UnityEngine;
 using Unity.Netcode;
 
-[RequireComponent(typeof(DetectShootingTarget))]
 public class PlayerShooting : NetworkBehaviour
 {
-    [SerializeField] private Transform bulletMuzzle;
+    [Header("Transforms")]
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private Transform shootingMuzzle;
 
+    [Header("Player's unit of ammunation")]
+    [SerializeField] private NetworkObject ammoPrefab;
+
+    [Header("The lenght of the clip will be the fire rate speed")]
     [SerializeField] private AnimationClip shootingAnimClip;
 
-    [SerializeField] private NetworkObject bulletPrefab;
+    [Header("The shooting distance")]
+    [SerializeField] private float shootingRadius;
 
-    [SerializeField] private PlayerStateController playerStateController;
-
-    [SerializeField] private Transform playerTransform;
-
-    private DetectShootingTarget shootingTarget;
-
-    public Collider aimTargetCollider;
-
-    private float shootingSpeed;
+    private float fireRate;
 
     private bool isShooting = false;
 
     private void Start()
     {
-        if (TryGetComponent<DetectShootingTarget>(out DetectShootingTarget _shootingTarget))
-        {
-            shootingTarget = _shootingTarget;
-        }
+        if (!IsOwner) Destroy(this);
 
-        shootingSpeed = shootingAnimClip.length;
-        shootingTarget.targetDetectedEvent += SetTarget;
+        fireRate = shootingAnimClip.length;
     }
 
-    private void SetTarget()
+    void Update()
     {
-        aimTargetCollider = shootingTarget.GetCurrentTargetCollider;
-        ShotTheTarget();
-    }
-
-    private void ShotTheTarget()
-    {
-        if ( playerStateController.GetState() == PlayerStates.Idle &&
-            aimTargetCollider != null && isShooting == false)
+        if (isShooting == false)
         {
-            isShooting = true;
-            float distance = Vector3.Distance(playerTransform.position, aimTargetCollider.transform.position);
+            Collider[] colliders = Physics.OverlapSphere(playerTransform.position, shootingRadius);
 
-            if (distance < shootingTarget.detectionRadius)
+            Transform closestTarget = null;
+            float distanceToClosestTarget = Mathf.Infinity;
+
+            foreach (Collider collider in colliders)
             {
-                Vector3 relativePosition = aimTargetCollider.transform.position - playerTransform.position;
-                Quaternion targetRotation = Quaternion.LookRotation(relativePosition, Vector3.up);
-                playerTransform.rotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
-
-                if (IsOwner)
+                if (collider.TryGetComponent<IAimTarget>(out IAimTarget _aimTarget))
                 {
-                    SpawnTheBulletServerRpc();
+                    float distance = Vector3.Distance(playerTransform.position, collider.transform.position);
+
+                    if (distance < distanceToClosestTarget)
+                    {
+                        closestTarget = collider.transform;
+                        distanceToClosestTarget = distance;
+                    }
                 }
             }
-            else
+
+            if(closestTarget != null)
             {
-                aimTargetCollider = null;
+                ShotTheTarget(closestTarget.position);
+                isShooting = true;
             }
-
-            Invoke(nameof(Reload), shootingSpeed);
         }
-
-        if (aimTargetCollider != null)
-            Invoke(nameof(ShotTheTarget), shootingSpeed);
-
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    private void SpawnTheBulletServerRpc()
+    private void ShotTheTarget(Vector3 targetPosition)
     {
-        NetworkObject bullet = (NetworkObject)Instantiate(bulletPrefab, bulletMuzzle.position, Quaternion.identity);
-        bullet.Spawn();
-    }
+        Vector3 relativePosition = targetPosition - playerTransform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+        playerTransform.rotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
 
+        print(targetPosition);
+
+        Invoke(nameof(Reload), fireRate);
+    }
 
     private void Reload() => isShooting = false;
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(playerTransform.position, shootingRadius);
+    }
 
-    private void OnDestroy() => shootingTarget.targetDetectedEvent -= SetTarget;
+
+    ///// <summary>
+    /////
+    ///// 
+    ///// </summary>
+    //[SerializeField] private Transform bulletMuzzle;
+
+    //[SerializeField] private AnimationClip shootingAnimClip;
+
+    //[SerializeField] private NetworkObject bulletPrefab;
+
+    //[SerializeField] private PlayerStateController playerStateController;
+
+    //[SerializeField] private Transform playerTransformOLD;
+
+    //private DetectShootingTarget shootingTarget;
+
+    //public Collider aimTargetCollider;
+
+    //private float shootingSpeed;
+
+    ////private bool isShooting = false;
+
+    //private void Start1()
+    //{
+    //    if (!IsOwner)
+    //    {
+    //        Destroy(shootingTarget);
+    //        Destroy(this);
+    //    }
+
+    //    if (TryGetComponent<DetectShootingTarget>(out DetectShootingTarget _shootingTarget))
+    //    {
+    //        shootingTarget = _shootingTarget;
+    //    }
+
+    //    shootingSpeed = shootingAnimClip.length;
+    //    shootingTarget.targetDetectedEvent += SetTarget;
+    //}
+
+    //private void SetTarget()
+    //{
+    //    aimTargetCollider = shootingTarget.GetCurrentTargetCollider;
+    //    ShotTheTarget();
+    //}
+
+    //private void ShotTheTarget()
+    //{
+    //    if (playerStateController.GetState() == PlayerStates.Idle &&
+    //        aimTargetCollider != null && isShooting == false)
+    //    {
+    //        isShooting = true;
+    //        float distance = Vector3.Distance(playerTransform.position, aimTargetCollider.transform.position);
+
+    //        if (distance < shootingTarget.detectionRadius)
+    //        {
+    //            Vector3 relativePosition = aimTargetCollider.transform.position - playerTransform.position;
+    //            Quaternion targetRotation = Quaternion.LookRotation(relativePosition, Vector3.up);
+    //            playerTransform.rotation = Quaternion.Euler(0f, targetRotation.eulerAngles.y, 0f);
+
+    //            print("Spanw of bullet");
+    //            SpawnTheBulletServerRpc(bulletMuzzle.position);
+    //        }
+    //        else
+    //        {
+    //            aimTargetCollider = null;
+    //        }
+
+    //        Invoke(nameof(Reload), shootingSpeed);
+    //    }
+
+    //    if (aimTargetCollider != null)
+    //        Invoke(nameof(ShotTheTarget), shootingSpeed);
+
+    //}
+
+    //[ServerRpc(RequireOwnership = false)]
+    //private void SpawnTheBulletServerRpc(Vector3 bulletMuzzlePosition)
+    //{
+    //    print("Here");
+    //    NetworkObject bullet = (NetworkObject)Instantiate(bulletPrefab, bulletMuzzlePosition, Quaternion.identity);
+    //    bullet.Spawn();
+    //}
+
+
+    //private void Reload() => isShooting = false;
+
+
+    //public override void OnDestroy()
+    //{
+    //    shootingTarget.targetDetectedEvent -= SetTarget;
+    //}
 }
