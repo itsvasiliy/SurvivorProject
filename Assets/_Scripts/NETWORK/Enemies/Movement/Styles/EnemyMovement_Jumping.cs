177125609c9f0f6104ae74a8603965fff0ca99a2
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-03-29 08:36:01 -0700

    Enemy jump movement style

--- Assets/_Scripts/NETWORK/Enemies/Movement/Styles/EnemyMovement_Jumping.cs
+++ Assets/_Scripts/NETWORK/Enemies/Movement/Styles/EnemyMovement_Jumping.cs
@@ -2,92 +2,164 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class EnemyMovement_Jumping : EnemyMovement
 {
-    [SerializeField] private AnimationCurve jumpCurve;
+    //[SerializeField] private AnimationCurve jumpCurve;
 
+    //[SerializeField] private float accelerationSpeed;
+
+
+    [SerializeField] private float jumpForce;
     [SerializeField] private float jumpHeight;
-    [SerializeField] private float accelerationSpeed;
     [SerializeField] private float jumpRate;
 
-    private Vector3 initialPosition;
-    private Vector3 landingPosition;
+    private Rigidbody _rigidbody;
 
-    private float jumpTime;
+    //private Vector3 initialPosition;
+    //private Vector3 landingPosition;
+
+    //private float jumpDuration;
+
 
     private void Start()
     {
-        Invoke(nameof(UpdateLandingPosition), jumpRate);
+        _rigidbody = GetComponent<Rigidbody>();
+
+        Invoke(nameof(Jumping), jumpRate);
     }
 
-    private Vector3 GetLandingPosition() // not done yet
+    private void Jumping()
     {
-        Vector3 landingDirection = Vector3.zero;
-        Vector3 calculatedLandingPosition = Vector3.zero;
-
         float randomDegreeAngle = 0;
 
         switch (Random.Range(0, 4))
         {
             case 0: // Forward
 
-                randomDegreeAngle = Random.Range(-30f, 30f);
+                //randomDegreeAngle = Random.Range(-30f, 30f);
+
+                randomDegreeAngle = 0f;
                 enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
-                landingDirection = Vector3.forward;
+                JumpForward();
 
                 break;
             case 1: // Backward
 
-                randomDegreeAngle = Random.Range(150f, 210f);
+                //randomDegreeAngle = Random.Range(150f, 210f);
+
+                randomDegreeAngle = 180f;
                 enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
-                landingDirection = Vector3.back;
+                JumpForward();
 
                 break;
             case 2: // Left
 
-                randomDegreeAngle = Random.Range(60f, 120f);
+                //randomDegreeAngle = Random.Range(60f, 120f);
+
+                randomDegreeAngle = -90f;
                 enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
-                landingDirection = Vector3.left;
+                JumpForward();
 
                 break;
             case 3: // Right
 
-                randomDegreeAngle = Random.Range(-120f, -60f);
+                //randomDegreeAngle = Random.Range(-120f, -60f);
+
+                randomDegreeAngle = 90f;
                 enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
-                landingDirection = Vector3.right;
+                JumpForward();
 
                 break;
             default:
                 break;
         }
 
-        return calculatedLandingPosition;
+        Invoke(nameof(Jumping), jumpRate);
     }
 
-    private void UpdateLandingPosition()
+    private void JumpForward()
     {
-        landingPosition = GetLandingPosition();
+        Vector3 jumpDirection = base.enemyTransform.forward * jumpForce;
+
+        _rigidbody.AddForce(jumpDirection, ForceMode.Impulse);
+        _rigidbody.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
     }
 
-    private void Update()
-    {
-        if (jumpTime <= 1f)
-        {
-            // Calculate the jump height using the curve
-            float height = jumpCurve.Evaluate(jumpTime) * jumpHeight;
 
-            Vector3 targetPosition = Vector3.Lerp(initialPosition, landingPosition, jumpTime) + Vector3.up * height;
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, accelerationSpeed * Time.deltaTime);
+    //private void Start()
+    //{
+    //    Invoke(nameof(UpdateLandingPosition), jumpRate);
+    //}
 
-            jumpTime += Time.fixedDeltaTime;
-        }
-        else
-        {
-            // Reset
-            jumpTime = 0f;
-            initialPosition = transform.position;
-        }
-    }
+    //private Vector3 GetLandingPosition() // not done yet
+    //{
+    //    Vector3 landingDirection = Vector3.zero;
+    //    Vector3 calculatedLandingPosition = Vector3.zero;
+
+    //    float randomDegreeAngle = 0;
+
+    //    switch (Random.Range(0, 4))
+    //    {
+    //        case 0: // Forward
+
+    //            randomDegreeAngle = Random.Range(-30f, 30f);
+    //            enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
+    //            landingDirection = Vector3.forward;
+
+    //            break;
+    //        case 1: // Backward
+
+    //            randomDegreeAngle = Random.Range(150f, 210f);
+    //            enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
+    //            landingDirection = Vector3.back;
+
+    //            break;
+    //        case 2: // Left
+
+    //            randomDegreeAngle = Random.Range(60f, 120f);
+    //            enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
+    //            landingDirection = Vector3.left;
+
+    //            break;
+    //        case 3: // Right
+
+    //            randomDegreeAngle = Random.Range(-120f, -60f);
+    //            enemyTransform.rotation = Quaternion.Euler(0f, randomDegreeAngle, 0f);
+    //            landingDirection = Vector3.right;
+
+    //            break;
+    //        default:
+    //            break;
+    //    }
+
+    //    return calculatedLandingPosition;
+    //}
+
+    //private void UpdateLandingPosition()
+    //{
+    //    landingPosition = GetLandingPosition();
+    //}
+
+    //private void Update()
+    //{
+    //    if (jumpDuration <= 1f)
+    //    {
+    //        // Calculate the jump height using the curve
+    //        float height = jumpCurve.Evaluate(jumpDuration) * jumpHeight;
+
+    //        Vector3 targetPosition = Vector3.Lerp(initialPosition, landingPosition, jumpDuration) + Vector3.up * height;
+    //        transform.position = Vector3.MoveTowards(transform.position, targetPosition, accelerationSpeed * Time.deltaTime);
+
+    //        jumpDuration += Time.fixedDeltaTime;
+    //    }
+    //    else
+    //    {
+    //        // Reset
+    //        jumpDuration = 0f;
+    //        initialPosition = transform.position;
+    //    }
+    //}
 
 
 }
