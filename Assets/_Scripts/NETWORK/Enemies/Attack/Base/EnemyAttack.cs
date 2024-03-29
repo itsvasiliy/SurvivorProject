63070713396e4b2b88b2c98c3d2512247bac512c
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-03-29 14:03:00 -0700

    EnemyMovement -  Following

--- Assets/_Scripts/NETWORK/Enemies/Attack/Base/EnemyAttack.cs
+++ Assets/_Scripts/NETWORK/Enemies/Attack/Base/EnemyAttack.cs
@@ -10,6 +10,5 @@ public class EnemyAttack : MonoBehaviour
         {
             // player get damage here...
         }
-
     }
 }
--- Assets/_Scripts/NETWORK/Enemies/Movement/Styles/EnemyMovement_Following.cs
+++ Assets/_Scripts/NETWORK/Enemies/Movement/Styles/EnemyMovement_Following.cs
@@ -1,8 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovement_Following : EnemyMovement
 {
-    
+    private NavMeshAgent _navMeshAgent;
+
+    private Transform detectedPlayer;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        detectedPlayer = GetClosestPlayer();
+
+        StartCoroutine(SearchForPlayer());
+        StartCoroutine(CheckPlayerDistance());
+    }
+
+    private IEnumerator SearchForPlayer()
+    {
+        while (true)
+        {
+            if (detectedPlayer == null)
+            {
+                detectedPlayer = GetClosestPlayer();
+
+                if(detectedPlayer != null)
+                {
+                    _navMeshAgent.SetDestination(detectedPlayer.position);
+                }
+            }
+            else
+            {
+                _navMeshAgent.SetDestination(detectedPlayer.position);
+            }
+
+
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+    private IEnumerator CheckPlayerDistance()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.1f);
+
+            if (detectedPlayer != null)
+            {
+                float distance = Vector3.Distance(detectedPlayer.position, base.enemyTransform.position);
+
+                if (distance > base.detectionRadius)
+                {
+                    detectedPlayer = null;
+                }
+            }
+        }
+    }
 }
