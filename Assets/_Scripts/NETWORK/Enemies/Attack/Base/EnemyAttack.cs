44ee757935d3c90e6ca7d3a249b003296b1e4306
Yuriy Balint <yurabalint0103@gmail.com>
2024-03-31 18:05:45 +0300

    Added melee attack for enemy

--- Assets/_Scripts/NETWORK/Enemies/Attack/Base/EnemyAttack.cs
+++ Assets/_Scripts/NETWORK/Enemies/Attack/Base/EnemyAttack.cs
@@ -1,14 +1,61 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] int bodyDamage;
+    [SerializeField] AnimationClip attackClip;
+
+    public Animator animator;
+
+    private float attackSpeed;
+
+    private bool isAttacking = false;
+
+
+    private void Start() => attackSpeed = attackClip.length;
+
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController player))
+        if (isAttacking) return;
+        Debug.Log(other.name);
+        if (other.gameObject.TryGetComponent<PlayerStateController>(out PlayerStateController player))
+            TryToAttack(other.gameObject);
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.TryGetComponent<PlayerStateController>(out PlayerStateController player))
+            StopToAttack();
+    }
+
+
+    private void TryToAttack(GameObject _gameobject)
+    {
+        try
         {
-            // player get damage here...
+            isAttacking = true;
+            animator.SetBool("IsAttacking", true);
+            StartCoroutine(DamagePlayerWithDelay(_gameobject.GetComponent<NetworkObjectHealth>()));
+            Invoke("ResetAttackStatus", attackSpeed);
+
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("NetworkObjectHealth not found in PlayerController object");
+            Debug.LogException(e);
         }
     }
+
+    private IEnumerator DamagePlayerWithDelay(NetworkObjectHealth _playerHealth)
+    {
+        yield return new WaitForSeconds(attackSpeed - 0.15f);
+        _playerHealth.GetDamage(bodyDamage);
+    }
+
+    private void StopToAttack() => animator.SetBool("IsAttacking", false);
+
+    private void ResetAttackStatus() => isAttacking = false;
 }
