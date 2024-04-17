40af5e093b2a77ad6333e3fa8f84ee19e6753abe
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-04-17 06:08:26 -0700

    Dragon enemy update

--- Assets/_Scripts/NETWORK/Enemies/Shooting/Styles/EnemyFireballShooting.cs
+++ Assets/_Scripts/NETWORK/Enemies/Shooting/Styles/EnemyFireballShooting.cs
@@ -5,17 +5,32 @@ using Unity.Netcode;
 
 public class EnemyFireballShooting : EnemyShooting, IEnemyShooting
 {
-    public void ShootTheBullet(Vector3 muzzleOfShot, Vector3 _targetPosition)
+//    NetworkObject
+
+//    private void Update() // test
+//    {
+//        if (Input.GetKeyUp(KeyCode.T))
+//        {
+//            ShootTheBullet(muzzleOfShot.position, muzzleOfShot.forward * 4);
+//        }
+//    }
+
+//    public void ShootTheBullet(Vector3 muzzleOfShot, Vector3 _targetPosition)
+//    {
+//        SpawnTheBulletServerRpc(muzzleOfShot);
+//    }
+
+//    [ServerRpc(RequireOwnership = false)]
+//    private void SpawnTheBulletServerRpc(Vector3 muzzleOfShot)
+//    {
+//        NetworkObject fireballClone = Instantiate(base.bullet.GetComponent<NetworkObject>(), muzzleOfShot, Quaternion.identity);
+//        fireballClone.Spawn();
+//    }
+
+    [ClientRpc]
+    private void SpawnTheBulletClientRpc(Vector3 muzzleOfShot, Vector3 target)
     {
-        SpawnTheBulletServerRpc(muzzleOfShot);
-
-
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void SpawnTheBulletServerRpc(Vector3 muzzleOfShot)
-    {
-        NetworkObject fireballClone = Instantiate(base.bullet, muzzleOfShot, Quaternion.identity);
-        fireballClone.Spawn();
+        bullet.GetComponent<Bullet>().SetTarget(target);
+        Instantiate(bullet, muzzleOfShot, bullet.transform.rotation);
     }
 }
