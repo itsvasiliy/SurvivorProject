0fcb98b61018baa561e7f85ef15ee5e45286418d
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-05-05 21:44:21 -0700

    Improvement of open structure menu button

--- Assets/_Scripts/Modification/UI/Structures/OpenStructuresBTN.cs
+++ Assets/_Scripts/Modification/UI/Structures/OpenStructuresBTN.cs
@@ -1,11 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OpenStructuresBTN : MonoBehaviour
 {
     [SerializeField] private GameObject structuresPanel;
 
+    [SerializeField] private Image _icon;
+
+    [SerializeField] private Sprite buildIcon;
+    [SerializeField] private Sprite closeIcon;
+
+    private bool isOpen = false;
+
     public void OnClick()
     {
-        structuresPanel.SetActive(!structuresPanel.activeSelf);
+        isOpen = !isOpen;
+
+        structuresPanel.SetActive(isOpen);
+
+        if(isOpen)
+        {
+            _icon.sprite = closeIcon;
+        }
+        else
+        {
+            _icon.sprite = buildIcon;
+        }
     }
 }
