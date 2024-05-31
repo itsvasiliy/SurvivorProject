d4c3b73a5b61326dfd2e74eafa89e1a740f9af74
Yuriy Balint <yurabalint0103@gmail.com>
2024-05-31 12:17:39 +0300

    Implemented validation for player change name option

--- Assets/_Scripts/NETWORK/Nickname/ChangeNickname.cs
+++ Assets/_Scripts/NETWORK/Nickname/ChangeNickname.cs
@@ -2,12 +2,18 @@ using TMPro;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeNickname : MonoBehaviour
 {
     [SerializeField] private TMP_Text nickname;
 
     [SerializeField] private TMP_InputField nickname_TMP_InputField;
+
+    [SerializeField] private GameObject invalidTextMessage;
+
+    [SerializeField] private Button saveBtn;
+
 
     private async void Start()
     {
@@ -21,14 +27,46 @@ public class ChangeNickname : MonoBehaviour
         LoadPlayerName();
     }
 
-    public async void LoadPlayerName()
-    {
-        nickname.text = await AuthenticationService.Instance.GetPlayerNameAsync();
-    }
+
+    public async void LoadPlayerName() => nickname.text = await AuthenticationService.Instance.GetPlayerNameAsync();
+    public void SetPlayerName(string _name) => nickname.text = _name; //use instead of LoadPlayerName because AuthenticationService has rate limit for requests
+
 
     public async void SaveNewNickname()
     {
-        await AuthenticationService.Instance.UpdatePlayerNameAsync(nickname_TMP_InputField.text);
-        LoadPlayerName();
+        var _name = nickname_TMP_InputField.text;
+        await AuthenticationService.Instance.UpdatePlayerNameAsync(_name);
+        SetPlayerName(_name);
+    }
+
+    
+    public void ValidateNickname()
+    {
+        var _name = nickname_TMP_InputField.text;
+
+        if (IsNameValid(_name))
+        {
+            invalidTextMessage.SetActive(false);
+            saveBtn.interactable = true;
+        }
+        else
+        {
+            invalidTextMessage.SetActive(true);
+            saveBtn.interactable = false;
+        }
+    }
+
+
+    //name must be null, contain spaces and should have a maximum length of 50 characters
+    private bool IsNameValid(string _name)
+    {
+        return !string.IsNullOrWhiteSpace(_name) && _name.Length <= 50 && !_name.Contains(" ");
+    }
+
+
+    public void ClearBoard()
+    {
+        nickname_TMP_InputField.text = "";
+        invalidTextMessage.SetActive(false);
     }
 }
