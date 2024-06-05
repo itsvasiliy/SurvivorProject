4c71273e6dfe9042b928e61732c3b59354da382d
Vasyl Kyrlyk <itsvasiliy@gmail.com>
2024-06-04 17:33:22 -0700

    Main menu play button scripting

--- Assets/_Scripts/NETWORK/UI/MainButton_MainMenu.cs
+++ Assets/_Scripts/NETWORK/UI/MainButton_MainMenu.cs
@@ -1,14 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 using Unity.Netcode;
-using UnityEngine.SceneManagement;
+
 
 public class MainButton_MainMenu : NetworkBehaviour
 {
-    public void StartGame()
+    [SerializeField] private GameObject lobbiesMenu;
+    [SerializeField] private GameObject createLobbyMenu;
+
+    public async void StartGame()
     {
-        NetworkManager.Singleton.StartHost();
-        NetworkManager.Singleton.SceneManager.LoadScene("Map", LoadSceneMode.Single);
+        try
+        {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            if (queryResponse.Results.Count == 0)
+            {
+                lobbiesMenu.SetActive(true);
+                createLobbyMenu.SetActive(true);
+            }
+            else
+            {
+                lobbiesMenu.SetActive(true);
+            }
+        }
+        catch(LobbyServiceException error)
+        {
+            Debug.LogError(error.Message);
+        }
     }
 }
