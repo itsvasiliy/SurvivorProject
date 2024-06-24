43b39edc57ec76016085986d23d2d9af1e1bb07b
Yuriy Balint <yurabalint0103@gmail.com>
2024-06-24 17:30:00 +0300

    Update ExitLobby.cs

--- Assets/_Scripts/NETWORK/Lobby/ExitLobby.cs
+++ Assets/_Scripts/NETWORK/Lobby/ExitLobby.cs
@@ -1,16 +1,22 @@
+using Unity.Netcode;
 using Unity.Services.Authentication;
 using Unity.Services.Lobbies;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExitLobby : MonoBehaviour
 {
+    NetworkObject player;
     public async void ExitLobbyButton()
     {
         try
         {
             string playerId = AuthenticationService.Instance.PlayerId;
             await LobbyService.Instance.RemovePlayerAsync(RelayServerDataManagerSingleton.lobbyId, playerId);
+
+            NetworkManager.Singleton.Shutdown();
         }
         catch (LobbyServiceException e)
         {
@@ -19,4 +25,7 @@ public class ExitLobby : MonoBehaviour
 
         SceneManager.LoadScene("MainMenu");
     }
+
+
+   
 }
